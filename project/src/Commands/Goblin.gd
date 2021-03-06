024334098e5b28e332ce70d8d26777fe91372924
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-03-06 09:08:36 -0500

    Move monster management to room

--- project/src/Commands/Goblin.gd
+++ project/src/Commands/Goblin.gd
@@ -6,5 +6,4 @@ func _init():
 
 func execute() -> void:
 	var goblin = preload("res://src/Monsters/Goblin.tscn").instance()
-	goblin.position = Vector2(300, 50)
-	GameState.room.add_child(goblin)
+	GameState.room.add_monster(goblin)
--- project/src/Room.gd
+++ project/src/Room.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var _monsters := $Monsters
 
 func _ready():
 	# Initialize the global state
@@ -10,3 +11,9 @@ func _ready():
 	card.command = load("res://src/Commands/Goblin.gd").new()
 	add_child(card)
 	card.position = Vector2(100,100)
+
+
+func add_monster(monster)->void:
+	_monsters.add_child(monster)
+	
+	
