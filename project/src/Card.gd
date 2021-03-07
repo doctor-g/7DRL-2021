93d1a1388f892c1e03c08fa7ec844617e988c9a2
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-03-06 21:35:04 -0500

    Track XP gained on card
    XP is gained by playing the card. Right now, the XP is not used for
    anything in particular.

--- project/src/Card.gd
+++ project/src/Card.gd
@@ -3,11 +3,12 @@ extends Control
 signal played
 
 var command 
-
+var xp := 0 setget _set_xp
 
 func _ready():
 	assert(command!=null, "Command may not be null")
 	$Button.text = command.name
+	_set_xp(0) # Trigger label update
 
 
 func update_playability(game:Game) -> void:
@@ -21,6 +22,12 @@ func can_play(game:Game)->bool:
 func play(game:Game) -> void:
 	assert(can_play(game))
 	command.execute(game)
+	_set_xp(xp+1)
+
+
+func _set_xp(value):
+	xp = value
+	$XpLabel.text = "XP: " + str(value)
 
 
 func _on_Button_pressed():
