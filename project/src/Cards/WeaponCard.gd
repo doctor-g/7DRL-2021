18dc49471ae9aaea6f59164407b80463705e03dd
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-03-12 13:47:30 -0500

    Add weapons

--- project/src/Cards/WeaponCard.gd
+++ project/src/Cards/WeaponCard.gd
@@ -4,17 +4,21 @@ var _weapons = {
 	1: [
 		["Dagger", "1d4", 324],
 		["Club", "1d6", 128],
-		["Bone", "1d6", 608]
+		["Bone Club", "1d4", 608],
+		["Staff", "1d6", 224],
+		["Pickaxe", "1d4+1", 283]
 	],
 	2: [
+		[ "Broadsword", "1d8", 420 ],
 		[ "Longsword", "1d8", 368 ],
 		[ "Mace", "2d4", 421 ],
-		[ "Spiked Club", "1d6+1", 129 ]
+		[ "Spiked Club", "1d6+1", 129 ],
+		[ "Axe", "1d8", 376]
 	],
 	3: [
-		[ "Magic Longsword", "1d8+2", 320],
-		[ "BattleAxe", "1d10", 375],
-		[ "Greatsword", "1d8+1", 465]
+		[ "Magic Longsword", "1d8+1", 320],
+		[ "Greataxe", "1d10", 375],
+		[ "Greatsword", "2d6", 465]
 	]
 }
 
